a5196760f9db405ec72f21db6b7b65aac6a35c37
sashkashishka <alexandr1696@hotmail.com>
2023-05-13 13:58:21 +0300

    feat: parse token from cookie for socket.io connection

--- apps/server/src/utils/getAuthToken.ts
+++ apps/server/src/utils/getAuthToken.ts
@@ -3,5 +3,15 @@ import type { IncomingHttpHeaders } from 'http';
 export function getAuthToken(headers: IncomingHttpHeaders) {
   const [type, token] = headers.authorization?.split(' ') ?? [];
 
+  if (!type) {
+    const [, v] =
+      headers.cookie
+        ?.split?.('; ')
+        ?.map?.((c) => c.split('='))
+        ?.find?.(([name]) => name === 'token') || [];
+
+    return v;
+  }
+
   return type === 'Token' ? token : undefined;
 }
